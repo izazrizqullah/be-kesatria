469f8db32bd26ae8de12b1f1481eebeed16972ff
izazrizqullah <izazrizqullah@gmail.com>
2025-03-19 06:21:26 +0700

    update: get all, get by id, update, delete profile

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -29,6 +29,107 @@ async function createProfile(req, res) {
   });
 }
 
+// get all new profile
+async function getAllProfile(req, res) {
+  const findData = await prisma.profile.findMany();
+
+  if (!findData) {
+    return res.status(404).json({
+      status: false,
+      message: "data profile not found",
+      data: [],
+    });
+  }
+
+  return res.status(200).json({
+    status: true,
+    message: "get data profile successfully",
+    data: findData,
+  });
+}
+
+// get profile by id
+async function getProfileById(req, res) {
+  const { id } = req.params;
+
+  const findData = await prisma.profile.findFirst({
+    where: { id: Number(id) },
+  });
+
+  if (!findData) {
+    return res.status(404).json({
+      status: false,
+      message: "data profile not found",
+    });
+  }
+
+  return res.status(200).json({
+    status: true,
+    message: "get profile by id successfull",
+    data: findData,
+  });
+}
+
+// update data profile by id
+async function updateProfileById(req, res) {
+  const { id } = req.params;
+  const { name, address, telp } = req.body;
+
+  const findData = await prisma.profile.findFirst({
+    where: { id: Number(id) },
+  });
+
+  if (!findData) {
+    return res.status(404).json({
+      status: false,
+      message: "data profile not found",
+    });
+  }
+
+  const profile = await prisma.profile.update({
+    where: { id: Number(id) },
+    data: {
+      name: name || findData.name,
+      address: address || findData.address,
+      telp: telp || findData.telp,
+    },
+  });
+
+  return res.status(200).json({
+    status: true,
+    message: "updated successfully",
+    data: profile,
+  });
+}
+
+// delete profile by id
+async function deleteProfileById(req, res) {
+  const { id } = req.params;
+
+  const findData = await prisma.profile.findFirst({
+    where: { id: Number(id) },
+  });
+
+  if (!findData) {
+    return res.status(404).json({
+      status: false,
+      message: "data profile not found",
+    });
+  }
+
+  const profile = await prisma.profile.delete({ where: { id: Number(id) } });
+
+  return res.status(200).json({
+    status: true,
+    message: "deleted successfully",
+    data: profile,
+  });
+}
+
 module.exports = {
   createProfile,
+  getAllProfile,
+  getProfileById,
+  updateProfileById,
+  deleteProfileById,
 };
--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const profileController = require("../controllers/profile.controller.js");
 
 router.post("/create", profileController.createProfile);
+router.get("/", profileController.getAllProfile);
+router.get("/:id", profileController.getProfileById);
+router.put("/:id", profileController.updateProfileById);
+router.delete("/:id", profileController.deleteProfileById);
 
 module.exports = router;
